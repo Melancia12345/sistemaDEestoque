b875d2951f05e72fd70ef8f440bd10dc51dfcbc3
Melancia12345 <152531161+Melancia12345@users.noreply.github.com>
2026-05-17 20:12:53 -0300

    adição de função de criar acdastro para produto e listar produto

--- estoque.py
+++ estoque.py
@@ -7,15 +7,31 @@ conexao = pyodbc.connect(
     "Trusted_Connection=yes;"
 )
 cursor = conexao.cursor()
+def cadastrar():
 
-nome_produto=input("digite o nome do produto:")
-quantidade_produto=int(input("digite a quantidade do produto:"))
-falor_produto=float(input("digite o preço do produto"))
-if nome_produto!=isalpha():
-    print("digite um nome valido!")
-cursor.execute(
-    "INSERT INTO produtos (nome, quantidade, preco) VALUES (?, ?, ?)",(nome_produto,quantidade_produto,falor_produto))
-conexao.commit()
 
-print("Produto cadastrado!")
+    nome_produto=input("digite o nome do produto:")
+    quantidade_produto=int(input("digite a quantidade do produto:"))
+    falor_produto=float(input("digite o preço do produto"))
+
+    cursor.execute(
+       "INSERT INTO produtos (nome, quantidade, preco) VALUES (?, ?, ?)",(nome_produto,quantidade_produto,falor_produto))
+    conexao.commit()
+
+    print("Produto cadastrado!")
+def listar():
+    cursor.execute("SELECT * FROM produtos")
+
+    for linha in cursor.fetchall():
+
+         print(linha)
+escolha=int(input( "escolha um numero uma obção 1 para cadastra produto e 2 para ver o estoque"   )) 
+if escolha==1:
+    cadastrar()     
+elif escolha==2:
+    listar()
+else:
+    print("nada")
+
+
 
